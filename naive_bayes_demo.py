6626ebb7ca55c84f815ef0d7d8aaee8b2e18609a
jazzsun10 <psun10@usfca.edu>
2023-12-17 00:58:14 +0700

    upload app file

---
+++ naive_bayes_demo.py
@@ -0,0 +1,68 @@
+# Required installations:
+# pip install streamlit scikit-learn numpy pandas
+import numpy as np
+import pandas as pd
+import streamlit as st
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
+
+st.title("Naive Bayes Classifier Demonstrator")
+
+st.write("""
+    ### Simple Text Classification Using Naive Bayes
+    This application demonstrates how the Naive Bayes classifier can be used to classify documents into different categories.
+    We use a simple dataset of short text documents for this demonstration.
+""")
+
+# Sample data
+data = {
+    "Text": ["This is a great movie", "This is a terrible movie", 
+             "I love this film", "I hate this film", 
+             "This movie is fantastic", "This movie is awful"],
+    "Category": ["Positive", "Negative", "Positive", "Negative", "Positive", "Negative"]
+}
+
+# Create a DataFrame
+df = pd.DataFrame(data)
+
+# Display the data
+st.write("#### Sample Data")
+st.dataframe(df)
+
+# Text preprocessing and vectorization
+vectorizer = CountVectorizer(stop_words='english')
+
+# Sidebar options
+st.sidebar.header("Model Settings")
+test_size = st.sidebar.slider("Test Size", 0.1, 0.9, 0.2, 0.1, help="Size of the test set.")
+
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(df['Text'], df['Category'], test_size=test_size, random_state=42)
+
+# Vectorize the text data
+X_train_counts = vectorizer.fit_transform(X_train)
+X_test_counts = vectorizer.transform(X_test)
+
+# Train a Naive Bayes classifier
+clf = MultinomialNB()
+clf.fit(X_train_counts, y_train)
+
+# Predictions
+predictions = clf.predict(X_test_counts)
+
+st.write("#### Predictions on Test Data")
+for doc, category in zip(X_test, predictions):
+    st.write(f"{doc} -> {category}")
+
+# Show accuracy
+accuracy = clf.score(X_test_counts, y_test)
+st.write(f"#### Accuracy: {accuracy:.2f}")
+
+st.write("""
+    ### How it Works
+    - **Text Preprocessing:** The documents are converted into a matrix of token counts (bag-of-words model).
+    - **Model Training:** A Naive Bayes classifier is trained on the processed text data.
+    - **Prediction:** The model predicts the category for new documents based on learned patterns.
+    - **Accuracy:** The proportion of test documents that are correctly classified by the model.
+""")
